fb5b527a38db369fc108f9c62e51713bbe162873
saha-7op <saha7pritam@gmail.com>
2025-10-03 14:23:41 +0530

    Header link + Image

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='absolute z-10 w-full'>
       <nav className='flex justify-between m-2 p-2'>
         <h1 className='font-bold text-xl'>Home</h1>
         <div className='flex gap-2'>
-            <h1>Performance</h1>
-            <h1>Relability</h1>
+            <Link href="/performance">Performance</Link>
+            <Link href='/relability'>Relability</Link>
         </div>
       </nav>
     </div>
